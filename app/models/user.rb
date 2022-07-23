e09ef87ce41c5612bc1ea04daaf703cf87f4f1a9
peppino708 <708.baseball.ster@gmail.com>
2022-07-23 11:39:52 +0900

    define association to relationship and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :active_relationships, foreign_key: :following_id, class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, foreign_key: :follower_id, class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :following
 end
