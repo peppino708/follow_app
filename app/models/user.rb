510399f49a11f18dff0270587b6e4f3b54573449
peppino708 <708.baseball.ster@gmail.com>
2022-07-23 11:43:17 +0900

    add comments to user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # フォローする側から中間テーブルへのアソシエーション
   has_many :active_relationships, foreign_key: :following_id, class_name: "Relationship", dependent: :destroy
+  # フォローする側からフォローされたユーザを取得する
   has_many :followings, through: :active_relationships, source: :follower
 
+  # フォローされる側から中間テーブルへのアソシエーション
   has_many :passive_relationships, foreign_key: :follower_id, class_name: "Relationship", dependent: :destroy
+  # フォローされる側からフォローしているユーザを取得する
   has_many :followers, through: :passive_relationships, source: :following
 end
