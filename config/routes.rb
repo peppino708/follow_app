043ef19eacc3d4786d9eca726ec5a5b0bf49be21
peppino708 <708.baseball.ster@gmail.com>
2022-07-23 11:51:57 +0900

    add relationships_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  resources :users
+  resources :users do
+    resources :relationships, only: [:create, :destroy]
+    # on: :memberと書くことで、ルーティングにidを持たせることができる
+    get :followings, on: :member
+    get :followers, on: :member
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
