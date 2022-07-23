c4ed052a84acf96852019777180c306a9337e7fa
peppino708 <708.baseball.ster@gmail.com>
2022-07-23 12:17:41 +0900

    create create and destroy action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,18 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    following = current_user.relationships.build(follower_id: params[:user_id])
+    follwing.save
+    # request.referrerと書くことによって、以前のパスに戻ることができる
+    redirect_to request.referrer || root_path
+  end
+
+  def destroy
+    following = current_user.relationships.find_by(follower_id: params[:user_id])
+    follwing.destroy
+    # request.referrerと書くことによって、以前のパスに戻ることができる
+    redirect_to request.referrer || root_path
+  end
+  # なぜ3行目や9行目の記述で自動的にfollwing_idにcurrent_userのidが入るのか？？
+  # →user.rbでhas_many :relationships, foreign_key: :following_idと記述しているため、current_user.relationshipsとすると自動的にfollowing_idにcurrent_userのidが入る
 end
