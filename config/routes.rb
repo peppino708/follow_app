9f26ddda3a8a64d279b940f8d1595ad3ee7d06eb
peppino708 <708.baseball.ster@gmail.com>
2022-07-24 10:55:14 +0900

    fix routes from resources to resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   resources :users do
-    resources :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
     # on: :memberと書くことで、ルーティングにidを持たせることができる
     get :followings, on: :member
     get :followers, on: :member
